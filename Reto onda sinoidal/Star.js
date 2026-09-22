c680e1aa345a2d66fec94d6ed0fc4064e941c7a1
k1ok-23 <hector.caloca@cetys.edu.mx>
2026-09-21 18:10:24 -0700

    Add files via upload

---
+++ Reto onda sinoidal/Star.js
@@ -0,0 +1,22 @@
+function Star () {
+  this.x = 0;
+  this.y = 0;
+  this.color = "#ffff00";
+  this.scaleX = 1;
+  this.scaleY = 1;
+  this.rotation = 0; 
+  this.planet = new Image();
+  this.planet.src = "star.png";
+  this.radius = this.planet.width/2;
+}
+
+Star.prototype.draw = function (context) {
+  context.save();
+  context.translate(this.x, this.y);
+  context.rotate(this.rotation);
+  context.scale(this.scaleX, this.scaleY);
+ 
+  context.drawImage(this.planet, 0, -this.planet.height/2);
+ 
+  context.restore();
+};
